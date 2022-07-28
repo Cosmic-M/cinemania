05d25180960da3d856af5689cdbde00fe1950e7c
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-07-28 19:09:31 +0300

    read file was composed, added init.db file

---
+++ src/main/java/mate.academy/Main.java
@@ -0,0 +1,84 @@
+package mate.academy;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.exception.RegistrationException;
+import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
+import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.service.CinemaHallService;
+import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
+
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) throws RegistrationException {
+        final MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        final CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
+        final MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
+        final UserService userService = (UserService) injector.getInstance(UserService.class);
+        final ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+        final OrderService orderService = (OrderService) injector.getInstance(OrderService.class);
+        final AuthenticationService authenticationService = (AuthenticationService) injector
+                .getInstance(AuthenticationService.class);
+
+        Movie fastAndFurious = new Movie("Fast and Furious");
+        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
+        movieService.add(fastAndFurious);
+        System.out.println(movieService.get(fastAndFurious.getId()));
+        movieService.getAll().forEach(System.out::println);
+
+        CinemaHall firstCinemaHall = new CinemaHall();
+        firstCinemaHall.setCapacity(100);
+        firstCinemaHall.setDescription("first hall with capacity 100");
+
+        CinemaHall secondCinemaHall = new CinemaHall();
+        secondCinemaHall.setCapacity(200);
+        secondCinemaHall.setDescription("second hall with capacity 200");
+
+        cinemaHallService.add(firstCinemaHall);
+        cinemaHallService.add(secondCinemaHall);
+
+        System.out.println(cinemaHallService.getAll());
+        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
+
+        MovieSession tomorrowMovieSession = new MovieSession();
+        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
+        tomorrowMovieSession.setMovie(fastAndFurious);
+        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
+
+        MovieSession yesterdayMovieSession = new MovieSession();
+        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
+        yesterdayMovieSession.setMovie(fastAndFurious);
+        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
+
+        movieSessionService.add(tomorrowMovieSession);
+        movieSessionService.add(yesterdayMovieSession);
+
+        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
+        System.out.println(movieSessionService.findAvailableSessions(
+                        fastAndFurious.getId(), LocalDate.now()));
+
+        User user = new User();
+        user.setEmail("special@gmail.com");
+        user.setPassword("secret");
+        user = authenticationService.register(user.getEmail(), user.getPassword());
+        shoppingCartService.addSession(tomorrowMovieSession, user);
+
+        orderService.completeOrder(shoppingCartService.getByUser(user));
+        List<Order> orders = orderService.getOrdersHistory(user);
+        orders.forEach(System.out::println);
+    }
+}
